a12a6721e459f0c741e45d0eea02c82a90af8871
SaicharanKandukuri <68287637+SaicharanKandukuri@users.noreply.github.com>
2020-08-27 16:00:38 +0530

    fixed some issues

--- setup files/setup-desktopl.sh
+++ setup files/setup-desktopl.sh
@@ -13,6 +13,11 @@ function Dependencies() {
     else
     printf "\n> whiptail    [\e[1;32m OK \e[0m]\n"
     fi
+    if [ ! $(command -v wget) ]; then
+    sudo apt install wget -y
+    else
+    printf "\n> wget  [\e[1;32m OK \e[0m]"
+    fi
     #########
     DE
 }
@@ -20,13 +25,18 @@ function DE(){
     printf "\n> <<<<Starting Desktop setup in 6 seconds.. "
     sleep 6
     printf "[\e[1;32m OK \e[0m]\n"
-    sudo apt install kali-linux-core kali-desktop-core kali-desktop-xfce -y || {
+    sudo apt install kali-linux-core kali-desktop-core -y || {
         printf "\n>   DE setup [\e[1;31m FAIL \e[0m]\n"
         printf "\n>   e[1;35mRetrying \e[1;32min 10 seconds!!\e[0m"
         Dependencies     
     }
+    if [ ! $(command -v qterminal) ]; then
+    sudo apt remove qtermianl -y
+    fi
+    sudo apt-get install gnome-terminal xfce* -y
     ## 
     printf "\n> DE setup [\e[1;32m DONE \e[0m]\n"
+    create_nonroot_user
     printf "\n> Setting up vnc server in 2 seconds\n"
     sleep 2
     sudo apt-get install tigervnc* -y
@@ -46,10 +56,15 @@ function DE(){
     Install_vlc
     installcode_oss
     Install_Libre
+    ##### Cleaning
+    sudo apt autoclean
+    sudo apt clean
+    sudo apt autoremove -y
+    printf "\e5m Installion Completed\e[0m\n"
 }
 setup_vnc_password() {
               ## Using whiptail for convinience
-			  vnc_passwd=$(whiptail --passwordbox "Please a short & sweet password to setup vnc minimum length 6 charecters" 0 50 --title "1x1 Toolloa VNC setup" 3>&1 1>&2 2>&3)
+			  vnc_passwd=$(whiptail --inputbox "Please a short & sweet password to setup vnc minimum length 6 charecters" 0 50 --title "1x1 Toolloa VNC setup" 3>&1 1>&2 2>&3)
               passwd_length=$(echo -n ${vnc_passwd} | wc -L)
 
 			  if ((${passwd_length} < 6)); then
@@ -62,10 +77,11 @@ setup_vnc_password() {
 			  printf "\n> your password is ${vnc_passwd}\n"
 			  mkdir -p ~/.vnc
 			  cd ~/.vnc
-			  echo "${vncpasswd}" | vncpasswd -f >passwd
+			  echo "${vnc_passwd}" | vncpasswd -f >passwd
 			  chmod 600 passwd
 			  printf "\n\e[1;32m you can type \e[5m\e[7m startvnc \e[0m to start vnc\n"
-			  printf "\n\e[1;32m you can type to \e[5m\e[7m stopvnc \e[0m vnc\n"
+			  printf "\n\e[1;32m you can type to \e[5m\e[7m stopvnc \e[0mto stop vnc-server\n"
+			  printf "\n \e[7m only works in root for now\e[0m\n"
 
 }
 Setup_startvnc() {
@@ -92,9 +108,9 @@ Setup_startvnc() {
 setup_stopvnc() {
 		cat > stopvnc <<-'EndOfFile'
 		#!/bin/bash
-		export USER="$(whoami)"
+		export USER="${whoami}"
 		export HOME="${HOME}"
-		CURRENT_PORT=$(cat /usr/local/bin/startvnc | grep '\-geometry' | awk -F ' ' '$0=$NF' | cut -d ':' -f 2 | tail -n 1)
+		CURRENT_PORT=$(cat /bin/startvnc | grep '\-geometry' | awk -F ' ' '$0=$NF' | cut -d ':' -f 2 | tail -n 1)
 		vncserver -kill :${CURRENT_PORT}
 		rm -rf /tmp/.X1-lock
 		rm -rf /tmp/.X11-unix/X1
@@ -105,9 +121,13 @@ setup_stopvnc() {
 installcode_oss() {
 		printf "\nsleeping 5 seconds before installing Code-oss"
 		printf "\n> Installing Visual studio (code-oss)...\n"
+		
         . <( wget -O - https://code.headmelted.com/installers/apt.sh )
+          rm -rf /etc/apt/sources.list.d/headmelted_vscode.list
 }
 Install_vlc() {
+    printf "\n> Installing VLC\n"
+    sleep 2
     if [ ! $(command -v vlc) ]; then
     sudo apt install vlc -y
     else
@@ -129,7 +149,7 @@ Install_chromium() {
 
 }
 Install_Libre() {
-    if (whiptail --title "1X1 Apps | Setup Libre Office Apps" --yesno "Dou want to install libre office apps.Download size 478mb Installation size 1580mb.Do you want to install?" 7 70); then
+    if (whiptail --title "1X1 Apps | Setup Libre Office Apps" --yesno "Dou want to install libre office apps.Download size 478mb Installation size 1580mb.Do you want to install?" 0 70); then
     sudo apt install libreoffice -y
     else
     printf "\n> User selected not to install  [\e[1;32m DONE \e[0m]"
@@ -149,9 +169,9 @@ create_nonroot_user() {
     if [ ! -d /home ]; then
     mkdier /home
     fi
-    USER_NAME=$(whiptail --title "1X1 SETUP | Add user" --inputbox "In order to run some majour applications like chromium you need to create a non user.Please enter desired name to create a user" 9 70 0 3>&1 1>&2 2>&3)
+    USER_NAME=$(whiptail --title "1X1 SETUP | Add user" --inputbox "In order to run some majour applications like chromium you need to create a non user.Please enter desired name to create a user" 0 70 0 3>&1 1>&2 2>&3)
     adduser --home /home/$USER_NAME --shell /bin/bash $USER_NAME
-    cat > sudoers <<- 'EndOfFile'
+    cat << EOF > sudoers
 
 #
 # This file MUST be edited with the 'visudo' command as root.
@@ -181,8 +201,8 @@ $USER_NAME    ALL=(ALL:ALL) ALL
 # See sudoers(5) for more information on "#include" directives:
 
 #includedir /etc/sudoers.d
-EndOfFile
-mv /etc/sudoers /etc/sudoers.1
+EOF
+rm -rf /etc/sudoers
 cp sudoers /etc/
 printf "\n> $USER_NAME IS added"
 
