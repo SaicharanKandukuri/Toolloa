146e1f1186c98bbf4b4e99013f2e480acf393429
SaicharanKandukuri <68287637+SaicharanKandukuri@users.noreply.github.com>
2020-08-28 13:59:13 +0530

    Update desktopl.sh

--- distro-plugins/desktopl.sh
+++ distro-plugins/desktopl.sh
@@ -40,8 +40,8 @@ distro_setup() {
     run_proot_cmd apt install sudo axel -y
     run_proot_cmd sudo apt upgrade -y
 	axel -o ~/setup-desktopl.sh https://raw.githubusercontent.com/1X1-Tech/Toolloa/master/setup%20files/setup-desktopl.sh
-    chmod +x setup-desktopl.sh
-	bash setup-desktopl.sh
+    chmod +x ~/setup-desktopl.sh
+	bash ~/setup-desktopl.sh
     
 }
 	
