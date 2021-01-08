c236a9524fec0771835f24ca34dac8127dfa845f
SaicharanKandukuri <68287637+1x1=SaicharanKandukuri@users.noreply.github.com>
2021-01-08 13:38:25 +0530

    fixed issue removed update & upgrades

--- distro-plugins/focal.sh
+++ distro-plugins/focal.sh
@@ -30,6 +30,9 @@ distro_setup() {
 	# Run command within proot'ed environment with
 	# run_proot_cmd function.
 	# Uncomment this to do system upgrade during installation.
-	 run_proot_cmd apt update
-	 run_proot_cmd apt upgrade -yq
+	# run_proot_cmd apt update
+	# run_proot_cmd apt upgrade -yq
+
+	:
+
 }
